4c7193a4ef9657885c2e8b15e96309ec584c2e2b
naveensharma <sharmanaveen0010@gmail.com>
2023-04-20 14:17:46 +0530

    commit new js and css files

---
+++ src/img.js
@@ -0,0 +1,24 @@
+import './style.css';
+const user = {
+    name: 'Hedy Lamarr',
+    imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+    imageSize: 90,
+  };
+  
+export default function Profile(){
+    return(
+        <>
+        <h1>{user.name}</h1>
+        <img 
+        className='avatar'
+        src={user.imageUrl}
+        alt={'this is the pic of '+ user.name}
+        style={{
+            height: user.imageSize,
+            width: user.imageSize
+        }}
+        />
+        </>
+    );
+}
+
