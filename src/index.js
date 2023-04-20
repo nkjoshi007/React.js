0be15904d5279ccc15753f2e9d83bb37068659dd
naveensharma <sharmanaveen0010@gmail.com>
2023-04-20 14:43:27 +0530

    commit index.js file

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 import Hello from './Helo';
+import Profile from './img';
+import Btn from './Clickbtn';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -11,6 +14,8 @@ root.render(
     
   <><App />
   <Hello />
+   <Btn />
+  <Profile />
   </>
   
 
