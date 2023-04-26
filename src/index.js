e105a856627529e703bfb490c6ed7b2448d9c018
naveensharma <sharmanaveen0010@gmail.com>
2023-04-26 17:46:48 +0530

    commit the carousel file

--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ import Respondevents from './Events_respond';
 import Mail from './Mail';
 import Diffinc from './Diffinc';
 import Profilechanger from './Proflechanger';
+import Carousel from './Carousel';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -37,6 +38,7 @@ root.render(
   <Mail/>
   <Diffinc/>
   <Profilechanger/>
+  <Carousel img1={"https://englishtribuneimages.blob.core.windows.net/gallary-content/2021/9/2021_9$largeimg_1725534029.JPG"} img2={"https://filmfare.wwmindia.com/content/2023/apr/salman-khan-11681297656.jpg"} img3={"https://lagatar24.com/wp-content/uploads/2023/04/Salman.jpg"} img4={"https://static.toiimg.com/thumb/msid-99492423,imgsize-54862,width-400,resizemode-4/99492423.jpg"}/>
   </>
   
 
