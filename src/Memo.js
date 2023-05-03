9eeecbaf62a9b0668c1aefcb7e1e5e915c2203c5
naveensharma <sharmanaveen0010@gmail.com>
2023-05-03 11:31:21 +0530

    commit usecallback and edit some code in useMemo

--- src/Memo.js
+++ src/Memo.js
@@ -1,21 +1,33 @@
-import { useMemo, useState } from "react"
-
+import { useCallback, useState } from "react"
+import Callback from "./Callback"
   
   
   const Memo=()=>{
     const[add,setAdd] = useState(0)
-  const Multiply=useMemo(()=>{
-    console.log("hi")
-    return add*10
-  },[add])
+    const[minus,setMinus] = useState(100)
+  // const Multiply=useMemo(()=>{
+  //   console.log("hi")
+  //   return minus*10
+  // },[minus])
+  const Func=useCallback(()=>{
+   //nothing to have
+  },[])
    return(
     <>
     <h1>{add}</h1>
     <button onClick={()=>setAdd(add+1)}>Add</button>
-    <button onClick={()=>setAdd(add-1)}>Minus</button>
-    <h1>:{Multiply}</h1>
-    <button onClick={Multiply}>Minus</button>
+    <h1>{minus}</h1>
+    <button onClick={()=>setMinus(minus-1)}>Minus</button>
+    {/* <h1>:{Multiply}</h1> */}
+    <Callback name={Func}/>
     </>
    )
   }
   export default Memo
+
+// useMemo
+// this is use memo where we use minimum one dependecies to rendering a functions when we use that component then that memo function will be rendering
+
+
+// useCallback
+// this is the hook which can stop the rendering fully or we can use dependencies for rendering the function, we have to use memo to bind the component with memo
